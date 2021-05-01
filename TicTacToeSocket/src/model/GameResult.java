2cfb55286d27ec2071f5bbef3a8fe9a2c02b55fb
Jiang Han <jianghan99@gmail.com>
2021-05-01 12:24:31 +0700

    Update GameResult Enum Values

--- TicTacToeSocket/src/model/GameResult.java
+++ TicTacToeSocket/src/model/GameResult.java
@@ -4,16 +4,7 @@ package model;
  * Hasil yang mungkin dari sebuah permainan
  */
 public enum GameResult {
-    /**
-     * Player 1 memenangkan permainan
-     */
-    P1WIN,
-    /**
-     * Player 2 memenangkan permainan
-     */
-    P2WIN, 
-    /**
-     * Permainan berakhir seri
-     */
+    WIN,
+    LOSE,
     DRAW;
 }
