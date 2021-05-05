ddd1828883c6e6ba4d6862d1072f157fef35c3f3
Jiang Han <jianghan99@gmail.com>
2021-05-05 22:05:16 +0700

    Add documentation in GameplayView Interface

--- TicTacToeSocket/src/view/GameplayView.java
+++ TicTacToeSocket/src/view/GameplayView.java
@@ -3,12 +3,36 @@ package view;
 import model.Board;
 
 /**
- *
+ * Interface untuk tampilan game
  */
 public interface GameplayView {
+    /**
+     * Method untuk menampilkan papan tic tac toe pada pengguna
+     * @param board data papan yang ingin digambar
+     */
     public void drawBoard(Board board);
+    
+    /**
+     * Method untuk menampilkan hasil permainan 
+     * @param result Hasil permainan
+     */
     public void showResult(String result);
+    
+    /**
+     * Method untuk mengupdate skor pemail
+     * @param selfScore Skor dari diri sendiri
+     * @param enemyScore Skor lawan
+     */
     public void updateScore(int selfScore, int enemyScore);
+    
+    /**
+     * Method ini dipanggil ketika program ingin dimulai
+     */
     public void start();
+    
+    /**
+     * Method ini dipanggil ketika program ingin dihentikan
+     * View harus berhenti dan tertutup setelah method ini dipanggil.
+     */
     public void stop();
 }
